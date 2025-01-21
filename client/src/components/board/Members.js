518737d0241d71736067a4e0cdb9f90b6cbd99ce
darpangohar47 <darpangohar47@gmail.com>
2025-01-21 16:47:38 +0530

    add server url

--- client/src/components/board/Members.js
+++ client/src/components/board/Members.js
@@ -23,7 +23,7 @@ const Members = () => {
   const handleInputValue = async (newInputValue) => {
     setInputValue(newInputValue);
     if (newInputValue && newInputValue !== '') {
-      const search = (await axios.get(`/api/users/${newInputValue}`)).data.slice(0, 5);
+      const search = (await axios.get(`https://trello-clone-4td9.onrender.com/api/users/${newInputValue}`)).data.slice(0, 5);
       setUsers(search && search.length > 0 ? search : []);
     }
   };
